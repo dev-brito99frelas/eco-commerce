85fcb7ec0fa7092220c9174ad8cc3fb6e54422e1
neto-b2c <clodoaldo.neto.116@ufrn.edu.br>
2022-04-29 23:41:18 -0300

    created splash screen

--- Android/app/src/main/java/com/example/btwocecocomerce/Activitys/SplashActivity.java
+++ Android/app/src/main/java/com/example/btwocecocomerce/Activitys/SplashActivity.java
@@ -2,15 +2,30 @@ package com.example.btwocecocomerce.Activitys;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+import com.example.btwocecocomerce.MainActivity;
 import com.example.btwocecocomerce.R;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity implements Runnable{
+    private final long TEMP = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Handler r1 = new Handler();
+        r1.postDelayed(this,this.TEMP);
+    }
+
+    @Override
+    public void run() {
+        Intent itnt = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(itnt);
+        finish();
+
     }
 }

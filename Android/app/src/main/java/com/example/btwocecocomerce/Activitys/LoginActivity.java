dbb4e554e7323978d852c7b160fe5a96d1f6c9f4
neto-b2c <clodoaldo.neto.116@ufrn.edu.br>
2022-04-30 13:38:25 -0300

    implemented start of onclick login button

--- Android/app/src/main/java/com/example/btwocecocomerce/Activitys/LoginActivity.java
+++ Android/app/src/main/java/com/example/btwocecocomerce/Activitys/LoginActivity.java
@@ -2,15 +2,32 @@ package com.example.btwocecocomerce.Activitys;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.btwocecocomerce.MainActivity;
 import com.example.btwocecocomerce.R;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private Button entrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        this.entrar = findViewById(R.id.bt_entrar);
+        configButtonEntrar();
+    }
+    public void  configButtonEntrar(){
+        this.entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }

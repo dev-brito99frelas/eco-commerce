29d7f3b74fc0d417d88a0c0c34d0e36785e6687d
neto-b2c <clodoaldo.neto.116@ufrn.edu.br>
2022-05-21 23:30:31 -0300

    started centaral Activyty

--- Android/app/src/main/java/com/example/btwocecocomerce/Activitys/Central.java
+++ Android/app/src/main/java/com/example/btwocecocomerce/Activitys/Central.java
@@ -9,23 +9,23 @@ import android.widget.Button;
 
 import com.example.btwocecocomerce.R;
 
-public class LoginActivity extends AppCompatActivity {
-    private Button entrar;
+public class Central extends AppCompatActivity {
+    Button bt_disponiveis;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_central);
 
-        this.entrar = findViewById(R.id.bt_entrar);
-        configButtonEntrar();
+        bt_disponiveis = findViewById(R.id.bt_areaDisponiveis);
+        confBotao();
     }
-    public void  configButtonEntrar(){
-        this.entrar.setOnClickListener(new View.OnClickListener() {
+    public void confBotao(){
+        this.bt_disponiveis.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), Central.class);
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(i);
-                finish();
             }
         });
     }
